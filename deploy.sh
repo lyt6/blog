1690a617f7161725c18f192b14544e46084cd14c
刘永涛 <liuyongtao@finupgroup.com>
2020-10-29 23:33:54 +0800

    add json

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yangxy6/blog.git master:gh-pages
+git push -f git@github.com:lyt6/blog.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,12 @@ module.exports = {
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1, // 可选的, 默认值是 1
       },
+        {
+            title: "json", // 必要的
+            path: "/json/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1, // 可选的, 默认值是 1
+        },
     ],
   },
   markdown: {
